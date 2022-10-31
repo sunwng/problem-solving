9602e0527bbd49d8b700d3bb88c175cc11a11c8e
sunwng <sunwgna@gmail.com>
2022-11-01 00:26:33 +0900

    2022-10-31

--- beakjoon/src/Main.java
+++ beakjoon/src/Main.java
@@ -4,17 +4,52 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    public static int[][] map;
+    public static boolean flag;
+    public static int N;
+    public static int M;
+
+    public static void dfs(boolean[] visit, int curNum, int targetNum) {
+        if (curNum == targetNum) {
+            flag = true;
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (flag) return;
+            if (map[curNum][i] == 0) continue;
+            if (visit[i]) continue;
+            visit[i] = true;
+            dfs(visit, i, targetNum);
+            visit[i] = false;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int H = Integer.parseInt(input[0]);
-        int W = Integer.parseInt(input[1]);
-
-        input = br.readLine().split(" ");
-        int pivot = Integer.parseInt(input[0]);
-        int result = 0;
-        int tempNum = 0;
-
-        
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        map = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            String[] temp = br.readLine().split(" ");
+            for (int j = 0; j < N; j++) {
+                int curNum = Integer.parseInt(temp[j]);
+                if (curNum == 1) {
+                    map[i][j + 1] = 1;
+                    map[j + 1][i] = 1;
+                }
+            }
+        }
+        String[] temp = br.readLine().split(" ");
+        for (int i = 0; i < M - 1; i++) {
+            flag = false;
+            boolean[] visit = new boolean[N + 1];
+            visit[Integer.parseInt(temp[i])] = true;
+            dfs(visit, Integer.parseInt(temp[i]), Integer.parseInt(temp[i + 1]));
+            if (!flag) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
 
     }
+}

e07f14fc56bf488d51f114ed4c0ec99f59fc1272
sunwng <sunwgna@gmail.com>
2022-11-04 22:06:17 +0900

    2022-11-04

--- beakjoon/src/bj_1027/Main.java
+++ beakjoon/src/bj_1027/Main.java
@@ -1,3 +1,5 @@
+package bj_1027;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
---
+++ beakjoon/src/bj_1976/Main.java
@@ -0,0 +1,67 @@
+package bj_1976;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int[][] map;
+    public static boolean flag;
+    public static int N;
+    public static int M;
+
+    public static int[] parent;
+
+    public static int find(int numIn) {
+        while (numIn != parent[numIn]) {
+            numIn = parent[numIn];
+        }
+        return numIn;
+    }
+
+    public static void apply(int i, int j) {
+        int parentI = find(i);
+        int parentJ = find(j);
+        if (parentI != parentJ) {
+            parent[parentJ] = parentI;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        map = new int[N + 1][N + 1];
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            String[] temp = br.readLine().split(" ");
+            for (int j = 1; j <= N; j++) {
+                int curNum = Integer.parseInt(temp[j-1]);
+                if (curNum == 1) {
+                    if (i < j) {
+                        apply(i, j);
+                    } else {
+                        apply(j, i);
+                    }
+                }
+            }
+        }
+
+        String[] temp = br.readLine().split(" ");
+        int prev = Integer.parseInt(temp[0]);
+        for (int i = 1; i < M; i++) {
+            int cur = Integer.parseInt(temp[i]);
+            if (find(prev) != find(cur)) {
+                System.out.println("NO");
+                return;
+            }
+            prev = cur;
+        }
+        System.out.println("YES");
+    }
+}

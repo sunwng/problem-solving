d90d94b42b36c301b2e34f2904b09762d956cddb
sunwng <sunwgna@gmail.com>
2022-11-09 23:11:32 +0900

    2022-11-09

---
+++ beakjoon/src/bj_1182/Main.java
@@ -0,0 +1,38 @@
+package bj_1182;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int target;
+    public static int[] map;
+    public static int count;
+    public static int N;
+
+    public static void find(int sum, int depth) {
+        if (depth == N) {
+            if (sum == target) count++;
+            return;
+        }
+        find(sum + map[depth], depth + 1);
+        find(sum, depth + 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] temp = br.readLine().split(" ");
+        N = Integer.parseInt(temp[0]);
+        target = Integer.parseInt(temp[1]);
+        temp = br.readLine().split(" ");
+        map = new int[N];
+        for (int i = 0; i < N; i++) {
+            map[i] = Integer.parseInt(temp[i]);
+        }
+        count = 0;
+        find(0, 0);
+        if (target == 0) count--;
+        System.out.println(count);
+    }
+}

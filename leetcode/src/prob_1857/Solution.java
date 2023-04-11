8d8f5c5cf62b1ce6c56862f08aa066ff88287bc1
sunwng <sunwgna@gmail.com>
2023-04-11 11:26:50 +0900

    2023-04-11: leetcode review

--- leetcode/src/prob_1857/Solution.java
+++ leetcode/src/prob_1857/Solution.java
@@ -5,14 +5,6 @@ import java.util.*;
 class Solution {
     int N;
 
-    int getMax(int[] target) {
-        int result = 0;
-        for (int i : target) {
-            result = Math.max(result, i);
-        }
-        return result;
-    }
-
     public int largestPathValue(String colors, int[][] edges) {
         N = colors.length();
         int[] indegrees = new int[N];
@@ -26,42 +18,38 @@ class Solution {
             map.get(edge[0]).add(edge[1]);
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-
+        int[][] dp = new int[N][26];
+        Queue<Integer> zeroInDegree = new ArrayDeque<>();
         for (int i = 0; i < N; i++) {
             if (indegrees[i] != 0) continue;
-            queue.add(i);
+            zeroInDegree.add(i);
+            dp[i][colors.charAt(i) - 'a'] = 1;
         }
 
-        int visit = 0;
+        int count = 0;
         int answer = 0;
-        int[][] count = new int[N][26];
-        for (int i = 0; i < N; i++) {
-            count[i][colors.charAt(i) - 'a']++;
-        }
+        while (!zeroInDegree.isEmpty()) {
+            int now = zeroInDegree.poll();
+            count++;
 
-        while (!queue.isEmpty()) {
-            int now = queue.poll();
-            visit++;
             for (int next : map.get(now)) {
-                for (int i = 0; i < 26; i++) {
-                    int adder = (colors.charAt(next) - 'a') == i ? 1 : 0;
-                    count[next][i] = Math.max(count[next][i], count[now][i] + adder);
-                }
-
                 indegrees[next]--;
-                if (indegrees[next] == 0) {
-                    queue.add(next);
+                if (indegrees[next] == 0) zeroInDegree.add(next);
+
+                for (int i = 0; i < 26; i++) {
+                    if (colors.charAt(next) - 'a' == i) {
+                        dp[next][i] = Math.max(dp[next][i], dp[now][i] + 1);
+                    } else {
+                        dp[next][i] = Math.max(dp[next][i], dp[now][i]);
+                    }
                 }
             }
-            answer = Math.max(answer, getMax(count[now]));
-            for (int[] temp : count) {
-                System.out.println(Arrays.toString(temp));
+
+            for (int i = 0; i < 26; i++) {
+                answer = Math.max(answer, dp[now][i]);
             }
-            System.out.println("==============");
+
         }
-
-        return (visit == N) ? answer : -1;
-
+        return count == N ? answer : -1;
     }
 }
